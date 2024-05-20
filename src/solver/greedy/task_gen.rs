bf85368a84626e35b40e100aadd145f73482d8a4
terry-u16 <terry-u16@users.noreply.github.com>
2024-05-21 02:53:35 +0900

    DPの計算式を変更

--- src/solver/greedy/task_gen.rs
+++ src/solver/greedy/task_gen.rs
@@ -183,7 +183,7 @@ pub fn generate_tasks(input: &Input, rng: &mut impl Rng) -> Result<Vec<Task>, &'
 }
 
 /// 滞留コンテナ数の最大値を最小にするDPを行い、搬入順（何番目にどの行から搬入するか）を返す
-fn dp(input: &Input) -> (u32, Vec<usize>) {
+fn dp(input: &Input) -> (u128, Vec<usize>) {
     let mut counts = vec![vec![vec![vec![vec![0; NP1]; NP1]; NP1]; NP1]; NP1];
 
     for i0 in 0..NP1 {
@@ -199,7 +199,7 @@ fn dp(input: &Input) -> (u32, Vec<usize>) {
         }
     }
 
-    let mut dp = vec![vec![vec![vec![vec![u32::MAX / 2; NP1]; NP1]; NP1]; NP1]; NP1];
+    let mut dp = vec![vec![vec![vec![vec![u128::MAX / 2; NP1]; NP1]; NP1]; NP1]; NP1];
     dp[0][0][0][0][0] = 0;
 
     let mut from = vec![vec![vec![vec![vec![([0; N], 0); NP1]; NP1]; NP1]; NP1]; NP1];
@@ -218,9 +218,10 @@ fn dp(input: &Input) -> (u32, Vec<usize>) {
             }
 
             let cnt = counts[indices[0]][indices[1]][indices[2]][indices[3]][indices[4]];
-            let new_cnt = current_dp.max(current_cnt).max(cnt);
+            let new_cnt = current_cnt.max(cnt);
+            let cost = current_dp + (1 << (new_cnt * 4));
 
-            if dp[indices[0]][indices[1]][indices[2]][indices[3]][indices[4]].change_min(new_cnt) {
+            if dp[indices[0]][indices[1]][indices[2]][indices[3]][indices[4]].change_min(cost) {
                 from[indices[0]][indices[1]][indices[2]][indices[3]][indices[4]] =
                     (old_indices, idx);
             }
